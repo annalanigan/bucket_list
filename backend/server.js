36fa47a70db4c8ab350cd61d0ed2c0aa36510bbe
BlueCanary777 <brian.n.connelly@gmail.com>
2018-02-07 13:10:41 +0000

    get route working

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,29 @@ const parser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 server.use(cors());
+
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+  if(err){
+    console.log(err);
+    return;
+  }
+  const db = client.db("bucket_list");
+
+  server.get("/api/countries", function(req, res){
+    db.collection("countries").find().toArray(function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(200);
+      console.log(result);
+      res.json(result);
+    })
+  });
+
+  server.listen(5000, function(){
+    console.log("listening on port 5000");
+  });
+
+});
