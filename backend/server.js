dac1e8e532bbecb1eba9b8910ab064d695cfba93
annalanigan <annalanigan@outlook.com>
2018-02-07 13:39:16 +0000

    post and delete all working

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 server.use(cors());
 
+server.use(parser.json());
+server.use(parser.urlencoded({extended: true}));
+
 MongoClient.connect("mongodb://localhost:27017", function(err, client){
   if(err){
     console.log(err);
@@ -21,9 +24,36 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client){
         res.send();
       }
       res.status(200);
-      console.log(result);
+      console.log("Showing all countries");
       res.json(result);
     })
+  });
+
+  server.delete("/api/countries", function(req, res){
+    db.collection("countries").deleteMany(function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(204);
+      console.log("Deleted all countries");
+      res.json(result);
+    });
+  });
+
+  server.post("/api/countries", function(req, res){
+    console.log(req.body);
+    db.collection("countries").save(req.body, function(err, result) {
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(201);
+      console.log("Created new country");
+      res.json(result.ops[0]);
+    });
   });
 
   server.listen(5000, function(){
